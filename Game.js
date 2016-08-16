f7719b635ffa61d2f2a3bb5c376a53d76d491e78
ijzm <ignacio.zevallos@hotmail.com>
2016-08-16 18:23:58 -0500

    Starting the big change!

--- Game.js
+++ Game.js
@@ -1,4 +1,4 @@
-AstroTrip.Game = function (game) {};
+AstroTrip.Game = function(game) {};
 
 var player;
 var explotion;
@@ -26,59 +26,58 @@ var soundbutton;
 
 AstroTrip.Game.prototype = {
 
-	preload: function () {
-	},
+	preload: function() {},
 
-	create: function () {
+	create: function() {
 		this.physics.startSystem(Phaser.Physics.ARCADE);
-		player = this.add.sprite(0,0, "char");
+		player = this.add.sprite(0, 0, "char");
 		player.anchor.x = 0.5;
 		player.anchor.y = 0.5;
 		this.physics.arcade.enable(player);
-		
+
 		boom = this.add.audio('boom');
 		newlevel = this.add.audio('newlevel');
 		collectcoin = this.add.audio('collectcoin');
 		click = this.add.audio("click");
 		noclick = this.add.audio("noclick");
-				
+
 		//parsing JSON :D
 		this.levels = JSON.parse(this.game.cache.getText("levels"));
-		player.x = this.levels.data[level].x *32-16;
-		player.y = this.levels.data[level].y *32-16;
-		map = this.add.tilemap(this.levels.data[level].file);
+		player.x = this.levels.data[level].x * 32 - 16;
+		player.y = this.levels.data[level].y * 32 - 16;
+		map = this.add.tilemap(level);
 		map.addTilesetImage('tiles', 'tiles');
 		layer = map.createLayer("layer");
 		layer.resizeWorld()
 		fuel = this.levels.data[level].fuel;
-		
-		fueltext = this.add.text(0,600, "Fuel: " + fuel, {
-        	font: "60px Arial",
+
+		fueltext = this.add.text(0, 600, "Fuel: " + fuel, {
+			font: "60px Arial",
 			//font: '24px Press Start 2P',
-        	fill: "#FFFFFF",
+			fill: "#FFFFFF",
 			stroke: '#000000',
 			strokeThickness: 3,
 		});
 		fueltext.fixedToCamera = true;
 		fueltext.anchor.y = 1;
-		
-		scoretext = this.add.text(800,600, "Score: " + score, {
-        	font: "60px Arial",
-        	fill: "#FFFFFF",
+
+		scoretext = this.add.text(800, 600, "Score: " + score, {
+			font: "60px Arial",
+			fill: "#FFFFFF",
 			stroke: '#000000',
 			strokeThickness: 3,
 		});
 		scoretext.anchor.x = 1;
 		scoretext.anchor.y = 1;
 		scoretext.fixedToCamera = true;
-		
-		
-		soundbutton = this.add.button(800,0,"soundbutton",this.togglesound,this)
+
+
+		soundbutton = this.add.button(800, 0, "soundbutton", this.togglesound, this)
 		soundbutton.anchor.x = 1;
 		soundbutton.anchor.y = 0;
 		soundbutton.fixedToCamera = true;
 		soundbutton.bringToTop();
-		
+
 		//walls
 		map.setTileIndexCallback(1, this.looselevel, this);
 		map.setTileIndexCallback(11, this.looselevel, this);
@@ -87,7 +86,7 @@ AstroTrip.Game.prototype = {
 		map.setTileIndexCallback(14, this.looselevel, this);
 		map.setTileIndexCallback(15, this.looselevel, this);
 		map.setTileIndexCallback(16, this.looselevel, this);
-		
+
 		map.setTileIndexCallback(9, this.looselevel, this);
 		//map.setTileIndexCallback(2, this., this);
 		//map.setTileIndexCallback(3, this., this);
@@ -102,153 +101,167 @@ AstroTrip.Game.prototype = {
 		map.setTileIndexCallback(24, this.bounceY, this);
 		//
 
-		
+
 		player.bringToTop();
-		
+
 		canclick = true;
 	},
 
-	update: function () {
+	update: function() {
 		//Making the VCAM
 		this.camera.follow(player);
 		//Adding collitions with map
 		this.physics.arcade.collide(player, layer);
-		
+
 		/*if (this.input.activePointer.isDown){
 			this.move();
 		}*/
-		
+
 		this.input.onTap.add(this.move, this);
-		
+
 		//console.log(this.input.worldX);
 		var keys = {
 			r: this.input.keyboard.addKey(Phaser.Keyboard.R),
-		} 
-		
-		if (keys.r.isDown && canclick){
+		}
+
+		if (keys.r.isDown && canclick) {
 			this.looselevel();
 		}
 	},
-	
-	render: function(){
-	},
-	
+
+	render: function() {},
+
 	move: function() {
-		if(fuel >= 1 && canclick){
-			if(this.input.y > 16){
-				player.rotation = this.physics.arcade.angleToPointer(player)+1.5;	
+		if (fuel >= 1 && canclick) {
+			if (this.input.y > 16) {
+				player.rotation = this.physics.arcade.angleToPointer(player) + 1.5;
 				player.body.velocity.setTo((this.input.worldX - player.x), (this.input.worldY - player.y));
 				fuel--;
 				this.updatetext();
-				if(playsound){
+				if (playsound) {
 					click.play();
 				}
 			}
-			
-			
+
+
 			lastX = player.body.velocity.x;
 			lastY = player.body.velocity.y;
 		} else {
-			if(playsound){
+			if (playsound) {
 				noclick.play();
 			}
 		}
-    },
-	
-	looselevel: function(){		
+	},
+
+	looselevel: function() {
 		canclick = false;
-		explotion = this.add.sprite(0,0, "explotion");
+		explotion = this.add.sprite(0, 0, "explotion");
 		explotion.anchor.x = 0.5;
 		explotion.anchor.y = 0.5;
 		explotion.x = player.x;
 		explotion.y = player.y;
-		
+
 		explotion.width = 0;
 		explotion.height = 0;
 
-    	this.add.tween(explotion).to( { width: 150, height: 150, }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
-		if(playsound){
+		this.add.tween(explotion).to({
+			width: 150,
+			height: 150,
+		}, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+		if (playsound) {
 			boom.play();
 		}
-		
+
 		player.kill();
-		
-		this.time.events.add(Phaser.Timer.SECOND * 1, function(){this.state.start("GameOver");}, this);
+
+		this.time.events.add(Phaser.Timer.SECOND * 1, function() {
+			this.state.start("GameOver");
+		}, this);
 	},
 
-	
-	winlevel: function(){
+
+	winlevel: function() {
 		canclick = false;
 		level++
 		newlevel.play();
-		if(localStorage.getItem('myItemKey') >= level){
-			
+		if (localStorage.getItem('myItemKey') >= level) {
+
 		} else {
 			localStorage.setItem('myItemKey', level.toString());
-		}		
-		
-		player.kill();
-		for (var i = fuel; i >= 1; --i){
-			
-			this.time.events.add(Phaser.Timer.SECOND * i/2, function(){ collectcoin.play(); score += 1000; fuel--; this.updatetext();}, this);
-			
 		}
-		this.time.events.add(Phaser.Timer.SECOND * (fuel + 1)/2, function(){this.state.start("LoadLevel");}, this);
+
+		player.kill();
+		for (var i = fuel; i >= 1; --i) {
+
+			this.time.events.add(Phaser.Timer.SECOND * i / 2, function() {
+				collectcoin.play();
+				score += 1000;
+				fuel--;
+				this.updatetext();
+			}, this);
+
+		}
+		this.time.events.add(Phaser.Timer.SECOND * (fuel + 1) / 2, function() {
+			this.state.start("LoadLevel");
+		}, this);
 	},
-	
-	collectstar: function(sprite, tile){
+
+	collectstar: function(sprite, tile) {
 		tile.index = 2;
 		score += 100;
 		this.updatetext();
 		layer.dirty = true;
-		if(playsound){
+		if (playsound) {
 			collectcoin.play();
 		}
 	},
-	
-	collecfuel: function(sprite, tile){
+
+	collecfuel: function(sprite, tile) {
 		tile.index = 2;
 		fuel++;
 		this.updatetext();
 		layer.dirty = true;
-		if(playsound){
+		if (playsound) {
 			collectcoin.play();
 		}
 	},
-	
-	updatetext: function(){
+
+	updatetext: function() {
 		fueltext.setText("Fuel: " + fuel);
 		scoretext.setText("Score: " + score)
 	},
-	
-	presbutton: function(sprite, tile){
+
+	presbutton: function(sprite, tile) {
 		tile.index = 8;
 		map.replace(9, 10);
 		layer.dirty = true;
 	},
-	
-	bounceY: function(){
-		player.body.velocity.setTo(-lastX,lastY);
+
+	bounceY: function() {
+		player.body.velocity.setTo(-lastX, lastY);
 		//player.rotation *= 2;
 		//player.angle += player.angle/2;
-		
-		this.time.events.add(Phaser.Timer.SECOND * .1, function(){lastX = player.body.velocity.x;
-		lastY = player.body.velocity.y;}, this);
-			
+
+		this.time.events.add(Phaser.Timer.SECOND * .1, function() {
+			lastX = player.body.velocity.x;
+			lastY = player.body.velocity.y;
+		}, this);
+
 	},
-	
-	bounceX: function(){
-		player.body.velocity.setTo(lastX,-lastY);
-		this.time.events.add(Phaser.Timer.SECOND * .1, function(){lastX = player.body.velocity.x;
-		lastY = player.body.velocity.y;}, this);
+
+	bounceX: function() {
+		player.body.velocity.setTo(lastX, -lastY);
+		this.time.events.add(Phaser.Timer.SECOND * .1, function() {
+			lastX = player.body.velocity.x;
+			lastY = player.body.velocity.y;
+		}, this);
 	},
-	togglesound: function(){
-		if(playsound){
+	togglesound: function() {
+		if (playsound) {
 			playsound = false;
-		} else if(playsound === false){
+		} else if (playsound === false) {
 			playsound = true;
 		}
 	},
 
 };
-
--- Preloader.js
+++ Preloader.js
@@ -1,29 +1,31 @@
-AstroTrip.Preloader = function (game) {  //declare the Preloader function
+AstroTrip.Preloader = function(game) { //declare the Preloader function
 
 	this.background = null;
 
 	this.ready = false;
-	
-	
+
+
 	WebFontConfig = {
 
-    //  'active' means all requested fonts have finished loading
-    //  We set a 1 second delay before calling 'createText'.
-    //  For some reason if we don't the browser cannot render the text the first time it's created.
-    active: function() { game.time.events.add(Phaser.Timer.SECOND, createText, this); },
+		//  'active' means all requested fonts have finished loading
+		//  We set a 1 second delay before calling 'createText'.
+		//  For some reason if we don't the browser cannot render the text the first time it's created.
+		active: function() {
+			game.time.events.add(Phaser.Timer.SECOND, createText, this);
+		},
 
-    //  The Google Fonts we want to load (specify as many as you like in the array)
-    google: {
-      families: ['Press Start 2P']
-    }
+		//  The Google Fonts we want to load (specify as many as you like in the array)
+		google: {
+			families: ['Press Start 2P']
+		}
 
-};
-	
+	};
+
 };
 
 AstroTrip.Preloader.prototype = {
 
-	preload: function () {
+	preload: function() {
 		//load all the required assets in the game - sprites, music, fonts,etc
 		this.load.image('playbutton', 'assets/playbutton.png');
 		this.load.image('creditsbutton', 'assets/creditsbutton.png');
@@ -31,55 +33,54 @@ AstroTrip.Preloader.prototype = {
 		this.load.image('helpbutton', 'assets/helpbutton.png');
 		this.load.image('menubutton', 'assets/menubutton.png');
 		this.load.image('retrybutton', 'assets/retrybutton.png');
-		this.load.spritesheet('button', 'assets/button.png',64,64);
+		this.load.spritesheet('button', 'assets/button.png', 64, 64);
 		this.load.image('buttonlocked', 'assets/buttonlocked.png');
 		this.load.image('soundbutton', 'assets/soundbutton.png');
 		this.load.image('logo', 'assets/logo.png');
-		
+
 		this.load.image('char', 'assets/player.png');
 		this.load.image('explotion', 'assets/explotion.png');
 		this.load.image('bg', 'assets/bg.png');
 		this.load.image('credits', 'assets/credits.png');
 		this.load.image('help', 'assets/help.png');
-		
 
-		this.load.tilemap('00', 'maps/00.json', null, Phaser.Tilemap.TILED_JSON);
-		this.load.tilemap('01', 'maps/01.json', null, Phaser.Tilemap.TILED_JSON);
-		this.load.tilemap('02', 'maps/02.json', null, Phaser.Tilemap.TILED_JSON);
-		this.load.tilemap('03', 'maps/03.json', null, Phaser.Tilemap.TILED_JSON);
-		this.load.tilemap('04', 'maps/04.json', null, Phaser.Tilemap.TILED_JSON);
-		this.load.tilemap('05', 'maps/05.json', null, Phaser.Tilemap.TILED_JSON);
-		this.load.tilemap('06', 'maps/06.json', null, Phaser.Tilemap.TILED_JSON);
-		this.load.tilemap('07', 'maps/07.json', null, Phaser.Tilemap.TILED_JSON);
-		this.load.tilemap('08', 'maps/08.json', null, Phaser.Tilemap.TILED_JSON);
-		this.load.tilemap('09', 'maps/09.json', null, Phaser.Tilemap.TILED_JSON);
+
+		this.load.tilemap('0', 'maps/0.json', null, Phaser.Tilemap.TILED_JSON);
+		this.load.tilemap('1', 'maps/1.json', null, Phaser.Tilemap.TILED_JSON);
+		this.load.tilemap('2', 'maps/2.json', null, Phaser.Tilemap.TILED_JSON);
+		this.load.tilemap('3', 'maps/3.json', null, Phaser.Tilemap.TILED_JSON);
+		this.load.tilemap('4', 'maps/4.json', null, Phaser.Tilemap.TILED_JSON);
+		this.load.tilemap('5', 'maps/5.json', null, Phaser.Tilemap.TILED_JSON);
+		this.load.tilemap('6', 'maps/6.json', null, Phaser.Tilemap.TILED_JSON);
+		this.load.tilemap('7', 'maps/7.json', null, Phaser.Tilemap.TILED_JSON);
+		this.load.tilemap('8', 'maps/8.json', null, Phaser.Tilemap.TILED_JSON);
+		this.load.tilemap('9', 'maps/9.json', null, Phaser.Tilemap.TILED_JSON);
 		this.load.tilemap('10', 'maps/10.json', null, Phaser.Tilemap.TILED_JSON);
 
 		this.load.image('tiles', 'assets/tiles.png');
 
 
 		this.load.audio('menumusic', 'assets/menumusic.mp3');
-		
+
 		this.load.audio('boom', 'assets/boom.mp3');
 		this.load.audio('collectcoin', 'assets/collectcoin.mp3');
 		this.load.audio('newlevel', 'assets/newlevel.mp3');
 		this.load.audio('click', 'assets/click.mp3');
 		this.load.audio('noclick', 'assets/noclick.mp3');
-		
+
 		this.load.text("levels", "levels.json");
-		
+
 		this.time.advancedTiming = true;
-		
-		
+
+
 	},
 
-		create: function () {
-	},
+	create: function() {},
 
-		update: function () {
+	update: function() {
 
 		//checking whether the music is ready to be played before proceeding to the Main Menu.
-		if (this.cache.isSoundDecoded('menumusic') && this.ready == false){
+		if (this.cache.isSoundDecoded('menumusic') && this.ready == false) {
 			this.ready = true;
 			this.state.start('MainMenu');
 		}
